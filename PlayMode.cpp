2b74145c218f4f25dda425526cc9d31a658f6f5d
Michelle Chang <mychang@andrew.cmu.edu>
2020-09-14 09:14:20 -0700

    changed set_palette to an auto

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -21,30 +21,36 @@ PlayMode::PlayMode() {
   std::vector< glm::u8vec4 > player_data2;
   load_png("bean2.png", &player_size2, &player_data2, player_origin);
 
+  auto set_palette = [this](int index, glm::uvec2 size, std::vector< glm::u8vec4 > data) {
+    glm::u8vec4 color0;
+    glm::u8vec4 color1;
+    glm::u8vec4 color2;
+    glm::u8vec4 color3;
+    int color_count = 0;
+    for (int j = 0; j < size[0] * size[1]; j++) {
+      glm::u8vec4 color = data[j];
+      if (color0 == color || color1 == color || color2 == color || color3 == color) continue;
+      std::cout<<glm::to_string(color);
+      if (color_count == 0) color0 = color;
+      else if (color_count == 1) color1 = color;
+      else if (color_count == 2) color2 = color;
+      else color3 = color;
+      color_count++;
+      if (color_count == 4) break;
+    }
+    ppu.palette_table[7] = {color0, color1, color2, color3};
+  };
 
-  int color_count = 0;
-  glm::u8vec4 color0;
-  glm::u8vec4 color1;
-  glm::u8vec4 color2;
-  glm::u8vec4 color3;
-  for (int j = 0; j < player_size[0] * player_size[1]; j++) {
-    glm::u8vec4 color = player_data[j];
-    if (color0 == color || color1 == color || color2 == color || color3 == color) continue;
-    std::cout<<glm::to_string(color);
-    if (color_count == 0) color0 = color;
-    else if (color_count == 1) color1 = color;
-    else if (color_count == 2) color2 = color;
-    else color3 = color;
-    color_count++;
-    if (color_count == 4) break;
-  }
+  set_palette(7, player_size, player_data);
 
-  ppu.palette_table[7] = {color0, color1, color2, color3};
-
-  auto set_bits = [this, color0, color1, color2, color3]
-          (int r0, int c0, int index, std::vector< glm::u8vec4 > data) {
+  auto set_bits = [this]
+          (int r0, int c0, int index, int palette_index, std::vector< glm::u8vec4 > data) {
     std::array< uint8_t, 8 > bit0;
     std::array< uint8_t, 8 > bit1;
+    glm::u8vec4 color0 = ppu.palette_table[palette_index][0];
+    glm::u8vec4 color1 = ppu.palette_table[palette_index][1];
+    glm::u8vec4 color2 = ppu.palette_table[palette_index][2];
+    glm::u8vec4 color3 = ppu.palette_table[palette_index][3];
     for (int r = 0; r < 8; r++) {
       for (int c = 0; c < 8; c++) {
         int player_index = (r0 + r) * 32 + (c0 + c);
@@ -71,14 +77,14 @@ PlayMode::PlayMode() {
   int index = 32;
   for (int i = 0; i < player_size[0]/8; i++) {
     for (int j = 0; j < player_size[1]/8; j++) {
-      set_bits(i*8, j*8, index, player_data);
+      set_bits(i*8, j*8, index, 7, player_data);
       index++;
     }
   }
   index = 48;
   for (int i = 0; i < player_size[0]/8; i++) {
     for (int j = 0; j < player_size[1]/8; j++) {
-      set_bits(i*8, j*8, index, player_data2);
+      set_bits(i*8, j*8, index, 7, player_data2);
       index++;
     }
   }
